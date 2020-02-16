643f4dcfbba241858371c066e558ca0680b9a58f
Xiang Chen <xchen@foqum.io>
2020-02-16 14:42:20 +0100

    Add logger

--- ortools_utils/model.py
+++ ortools_utils/model.py
@@ -11,12 +11,15 @@ def export_proto(model: cp_model.CpModel, filepath):
         f.write(str(model.Proto()))
 
 
-def log_num_vars(model: cp_model.CpModel, step_name: str, level='debug'):
+def log_num_vars(
+    model: cp_model.CpModel, step_name: str, level='debug', logger=None
+):
     level = level.lower().strip()
     level_options = ['debug', 'info', 'warning', 'error', 'critical']
     if level not in level_options:
         raise Exception(f'Unknown level "{level}", {level_options}')
-    getattr(logging, level)(
+    logger = logger or logging
+    getattr(logger, level)(
         f'"{step_name}": '
         f'Vars: {len(model.Proto().variables)}, '
         f'Constr: {len(model.Proto().constraints)}'
